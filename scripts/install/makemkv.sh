6c72f96c50d4bfc26e95cc1c91ce841dec0a4d04
ts050 <vivek.vanka1996@gmail.com>
2019-02-22 22:16:29 +0530

    Initial commit - WIP dirty code

---
+++ scripts/install/makemkv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ -f /tmp/.install.lock ]]; then
+  OUTTO="/root/logs/install.log"
+elif [[ -f /install/.panel.lock ]]; then
+  OUTTO="/srv/panel/db/output.log"
+else
+  OUTTO="/dev/null"
+fi
+
+function _installMakemkvDependencies() {
+    
+}
