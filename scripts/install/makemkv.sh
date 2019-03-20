cf5e0c85e36618fd1793c7051f8b10f6e5d236c4
ts050 <vivek.vanka1996@gmail.com>
2019-03-20 20:47:53 +0530

    fixes to makemkv scripts

--- scripts/install/makemkv.sh
+++ scripts/install/makemkv.sh
@@ -11,42 +11,45 @@ fi
 function _updateMakemkvLicenseKey() {
   key=$(wget -q "https://www.makemkv.com/forum/viewtopic.php?f=5&t=1053" -O - | grep code | sed -n 's:.*<code>\(.*\)</code>.*:\1:p')
   if [[ ! -f ~/.MakeMKV/settings.conf ]]; then
+    mkdir ~/.MakeMKV
     touch ~/.MakeMKV/settings.conf
   fi
-  echo "app_Key = "$key"" >~/.MakeMKV/settings.conf
+  echo "app_Key = \"$key\"" >~/.MakeMKV/settings.conf
 }
 
 function _installMakemkvDependencies() {
-  apt-get install build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev
+  apt-get install -yqq build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev libdvdnav4 libdvdread4 libudev-dev >/dev/null 2>&1
 }
 
 url=$(wget -q "https://www.makemkv.com/forum/viewtopic.php?f=3&t=224" -O - | grep -E "\/download\/" | sed -n 's/.*href="\([^"]*\).*/\1/p')
 
 if [[ ! -d /opt ]]; then mkdir /opt; fi
 cd /opt
-wget $(echo $url | awk '{print $1}')
-wget $(echo $url | awk '{print $2}')
+wget -q $(echo $url | awk '{print $1}')
+wget -q $(echo $url | awk '{print $2}')
 find . -mindepth 1 -maxdepth 1 -name "make*.gz" -exec tar xzf {} \; 2>/dev/null
-rm -f makemkv*.gz
+rm -f makemkv*.gz >/dev/null 2>&1
 
 _installMakemkvDependencies
 # makemkv-oss install
 cd makemkv-oss-*
-./configure
-make
-sudo make install
+./configure --disable-gui >/dev/null 2>&1
+make --silent
+sudo make --silent install >/dev/null 2>&1
 
 # makemkv-bin install
 cd ../makemkv-bin-*
+
 # Skipping accept license step
 sed -i 's/all: tmp\/eula_accepted/all:/g' Makefile
-sudo make install
+sudo make --silent install
 rm -rf makemkv-*
+
 # TODO - Add option to build using ffmpeg
-if [[ ! -f /install/.ffmpeg.lock ]]; then
-  echo "ERROR: no ffmpeg installation. Please install ffmpeg if you want to configure your installation."
-  exit 1
-fi
+#if [[ ! -f /install/.ffmpeg.lock ]]; then
+#  echo "ERROR: no ffmpeg installation. Please install ffmpeg if you want to configure your installation."
+#  exit 1
+#fi
 
 _updateMakemkvLicenseKey
 
